d01843708a34007ec05e36f6e460a97c93fc455f
j500 <C026730@cht-dm.cht.canon.co.th>
2024-10-11 10:22:22 +0700

    0.1.0

--- hexss/__init__.py
+++ hexss/__init__.py
@@ -1,1 +1,2 @@
-from .json.json_utils import json_load, json_dump, json_update
+from .json import json_load, json_dump, json_update
+from .multiprocessing import list_to_manager_list, dict_to_manager_dict

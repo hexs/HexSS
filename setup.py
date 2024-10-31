6127cfb9fdaad6c7b67ad85bde8dfe0aec650e38
j500 <C026730@cht-dm.cht.canon.co.th>
2024-10-31 12:01:38 +0700

    0.7.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hexss',
-    version='0.6.1',
+    version='0.7.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],

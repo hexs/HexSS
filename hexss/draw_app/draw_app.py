6f39420990c6a0d7bb70fddea25668e194217296
j500 <C026730@cht-dm.cht.canon.co.th>
2024-11-13 14:22:36 +0700

    update draw_app

--- hexss/draw_app/draw_app.py
+++ hexss/draw_app/draw_app.py
@@ -1,25 +1,82 @@
-from flask import Flask, render_template, send_from_directory, jsonify
+# app.py
+from datetime import datetime
+import cv2
+import numpy as np
+from flask import Flask, render_template, jsonify, request, Response
 import os
 
+
+class Video:
+    def __init__(self, path):
+        self.path = path
+        self.cap = None
+        self.total_frames = self.get_total_frames()
+        self.frame_number = 0
+
+    def get_total_frames(self):
+        self.cap = cv2.VideoCapture(self.path)
+        total = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        return total
+
+    def get_img(self):
+        if 0 <= self.frame_number < self.total_frames:
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_number)
+            return self.cap.read()[1]
+
+
+video = None
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    images_list = os.listdir('images')
-    valid_extensions = ['.jpg', '.jpeg', '.png', '.gif']
-    images_list = [img for img in images_list if any(img.lower().endswith(ext) for ext in valid_extensions)]
-    return render_template('index.html', images=images_list)
+    videos = os.listdir('data')
+    return render_template('index.html', videos=videos)
 
-@app.route('/images/<path:filename>')
-def serve_image(filename):
-    return send_from_directory('images', filename)
 
-@app.route('/get_images')
-def get_images():
-    images_list = os.listdir('images')
-    valid_extensions = ['.jpg', '.jpeg', '.png', '.gif']
-    images_list = [img for img in images_list if any(img.lower().endswith(ext) for ext in valid_extensions)]
-    return jsonify(images_list)
+@app.route('/api/setup_video')
+def setup_video():
+    global video
+
+    file_name = request.args.get('name', default='', type=str)
+    if file_name and file_name in os.listdir('data'):
+        video = Video(os.path.join('data', file_name))
+        print('video.total_frames', video.total_frames)
+        return jsonify({
+            'success': True,
+            'total_frames': video.total_frames
+        })
+    return jsonify({'success': False, 'error': 'Invalid video file'})
+
+
+@app.route('/api/set_frame_number')
+def set_frame_number():
+    global video
+    if video is None:
+        return "setup_video"
+    video.frame_number = request.args.get('frame', default=0, type=int)
+    return jsonify({'success': True, 'frame': video.frame_number})
+
+
+@app.route('/api/get_img')
+def get_video():
+    def generate():
+        if video is None:
+            return "setup_video"
+        while True:
+            img = video.get_img()
+            if img is None:
+                img = np.full((480, 640, 3), (50, 50, 50), dtype=np.uint8)
+                cv2.putText(img, f'img = None', (30, 50), 1, 2, (0, 0, 255), 2)
+                cv2.putText(img, datetime.now().strftime('%Y-%m-%d  %H:%M:%S'), (30, 130), 1, 2, (0, 0, 255), 2)
+
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run('0.0.0.0', port=5002, debug=True)
--- hexss/draw_app/static/script.js
+++ hexss/draw_app/static/script.js
@@ -1,3 +1,4 @@
+// static/script.js
 const canvas = document.getElementById('imageCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -8,54 +9,72 @@ let offsetY = 0;
 let isDragging = false;
 let lastX, lastY;
 
-let images = [];
-let currentImageIndex = 0;
+let totalFrames = 0;
+let currentFrame = 0;
 
-fetch('/get_images')
-    .then(response => response.json())
-    .then(data => {
-        images = data;
-        populateImageList();
-        if (images.length > 0) {
-            loadImage('/images/' + images[0]);
-        }
-    });
+const videoSelect = document.getElementById('videoSelect');
+const frameSlider = document.getElementById('frameSlider');
+const frameNumber = document.getElementById('frameNumber');
 
-function populateImageList() {
-    const imageList = document.getElementById('imageList');
-    imageList.innerHTML = '';
-    images.forEach((img, index) => {
-        const option = document.createElement('option');
-        option.value = index;
-        option.textContent = img;
-        imageList.appendChild(option);
-    });
+// Function to set canvas size
+function setCanvasSize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
 
-function loadImage(src) {
+// Initial canvas size setup
+setCanvasSize();
+
+videoSelect.addEventListener('change', () => {
+    console.log('load video')
+    const selectedVideo = videoSelect.value;
+    if (selectedVideo) {
+        fetch(`/api/setup_video?name=${selectedVideo}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    totalFrames = data.total_frames;
+                    frameSlider.max = totalFrames - 1;
+                    currentFrame = 0;
+                    frameSlider.value = 0;
+                    updateFrameNumber();
+                    loadFrame();
+                } else {
+                    alert('Failed to load video');
+                }
+            })
+            .catch(error => {
+                console.error('Error loading video:', error);
+                alert('Error loading video. Please try again.');
+            });
+    }
+});
+
+frameSlider.addEventListener('input', () => {
+    currentFrame = parseInt(frameSlider.value);
+    updateFrameNumber();
+    fetch(`/api/set_frame_number?frame=${currentFrame}`)
+        .then(response => response.json())
+        .catch(error => {
+            console.error('Error setting frame number:', error);
+        });
+});
+
+function updateFrameNumber() {
+    frameNumber.textContent = `Frame: ${currentFrame} / ${totalFrames - 1}`;
+}
+
+function loadFrame() {
     img = new Image();
-    img.src = src;
     img.onload = function () {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
         scale = 1;
         offsetX = 0;
         offsetY = 0;
-        draw();
     };
+    img.src = `/api/get_img`;
 }
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.scale(scale, scale);
-    ctx.translate(-img.width / 2 + offsetX / scale, -img.height / 2 + offsetY / scale);
-    ctx.drawImage(img, 0, 0);
-    ctx.restore();
-}
-
-canvas.addEventListener('wheel', (e) => {
+function handleZoom(e) {
     e.preventDefault();
     const zoomIntensity = 0.1;
     const mouseX = e.clientX - canvas.offsetLeft;
@@ -66,11 +85,9 @@ canvas.addEventListener('wheel', (e) => {
     scale *= zoom;
     offsetX += (mouseX - canvas.width / 2 - offsetX) * (1 - zoom);
     offsetY += (mouseY - canvas.height / 2 - offsetY) * (1 - zoom);
+}
 
-    draw();
-});
-
-canvas.addEventListener('mousedown', (e) => {
+function startDrag(e) {
     if (e.button === 1) { // Middle mouse button
         e.preventDefault();
         isDragging = true;
@@ -78,9 +95,9 @@ canvas.addEventListener('mousedown', (e) => {
         lastY = e.clientY;
         canvas.style.cursor = 'grabbing';
     }
-});
+}
 
-canvas.addEventListener('mousemove', (e) => {
+function drag(e) {
     if (isDragging) {
         const deltaX = e.clientX - lastX;
         const deltaY = e.clientY - lastY;
@@ -88,47 +105,45 @@ canvas.addEventListener('mousemove', (e) => {
         offsetY += deltaY;
         lastX = e.clientX;
         lastY = e.clientY;
-        draw();
     }
-});
+}
 
-canvas.addEventListener('mouseup', () => {
+function stopDrag() {
     isDragging = false;
     canvas.style.cursor = 'default';
-});
+}
 
-canvas.addEventListener('mouseleave', () => {
-    isDragging = false;
-    canvas.style.cursor = 'default';
-});
+canvas.addEventListener('wheel', handleZoom);
+canvas.addEventListener('mousedown', startDrag);
+canvas.addEventListener('mousemove', drag);
+canvas.addEventListener('mouseup', stopDrag);
+canvas.addEventListener('mouseleave', stopDrag);
+window.addEventListener('resize', setCanvasSize);
 
-window.addEventListener('resize', () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (img.complete && img.naturalWidth !== 0) {
+        ctx.save();
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.scale(scale, scale);
+        ctx.translate(-img.width / 2 + offsetX / scale, -img.height / 2 + offsetY / scale);
+        ctx.drawImage(img, 0, 0);
+        ctx.restore();
+    }
+}
+
+let animationId;
+
+function animate() {
     draw();
-});
+    animationId = requestAnimationFrame(animate);
+}
 
-document.getElementById('openSelector').addEventListener('click', () => {
-    document.getElementById('imageSelector').style.display = 'block';
-});
+animate();
+updateFrameNumber();
 
-document.getElementById('closeSelector').addEventListener('click', () => {
-    document.getElementById('imageSelector').style.display = 'none';
-});
-
-document.getElementById('prevImage').addEventListener('click', () => {
-    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-    loadImage('/images/' + images[currentImageIndex]);
-});
-
-document.getElementById('nextImage').addEventListener('click', () => {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    loadImage('/images/' + images[currentImageIndex]);
-});
-
-document.getElementById('loadImage').addEventListener('click', () => {
-    const selectedIndex = document.getElementById('imageList').value;
-    currentImageIndex = parseInt(selectedIndex);
-    loadImage('/images/' + images[currentImageIndex]);
-    document.getElementById('imageSelector').style.display = 'none';
-});
+function stopAnimation() {
+    if (animationId) {
+        cancelAnimationFrame(animationId);
+    }
+}

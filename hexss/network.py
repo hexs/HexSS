f930613ff8335a82d49baf9ad8b2d1fc4331648f
j500 <C026730@cht-dm.cht.canon.co.th>
2024-11-21 14:55:28 +0700

    0.9.12

--- hexss/network.py
+++ hexss/network.py
@@ -3,7 +3,6 @@ import socket
 from socket import AddressFamily
 import subprocess
 import platform
-import webbrowser
 
 
 def get_hostname() -> str:
@@ -17,22 +16,83 @@ def get_ipv4() -> str:
 def get_ips():
     ipv4 = []
     ipv6 = []
-    for item in socket.getaddrinfo(socket.gethostname(), None):
-        protocol, *_, (ip, *_) = item
-        if protocol == AddressFamily.AF_INET:
-            ipv4.append(ip)
-        elif protocol == AddressFamily.AF_INET6:
-            ipv6.append(ip)
+    if platform.system() == "Windows":
+        for item in socket.getaddrinfo(socket.gethostname(), None):
+            protocol, *_, (ip, *_) = item
+            if protocol == AddressFamily.AF_INET:
+                ipv4.append(ip)
+            elif protocol == AddressFamily.AF_INET6:
+                ipv6.append(ip)
+
+    elif platform.system() == "Linux":
+        RTM_NEWADDR = 20
+        RTM_GETADDR = 22
+        NLM_F_REQUEST = 0x01
+        NLM_F_ROOT = 0x100
+        s = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW)
+        req = (
+            # nlmsghdr
+                int.to_bytes(0, 4, 'little', signed=False) +  # nlmsg_len
+                int.to_bytes(RTM_GETADDR, 2, 'little', signed=False) +  # nlmsg_type
+                int.to_bytes(NLM_F_REQUEST | NLM_F_ROOT, 2, 'little', signed=False) +  # nlmsg_flags
+                int.to_bytes(0, 2, 'little', signed=False) +  # nlmsg_seq
+                int.to_bytes(0, 2, 'little', signed=False) +  # nlmsg_pid
+                # ifinfomsg
+                b'\0' * 8
+        )
+        req = int.to_bytes(len(req), 4, 'little') + req[4:]
+        s.sendall(req)
+        full_resp = s.recv(4096)
+        while full_resp:
+            resp = full_resp
+            # nlmsghdr
+            nlmsg_len = int.from_bytes(resp[0:4], 'little', signed=False)
+            nlmsg_type = int.from_bytes(resp[4:6], 'little', signed=False)
+            assert not nlmsg_len % 4, nlmsg_len
+            resp = resp[16:nlmsg_len]
+            full_resp = full_resp[nlmsg_len:]
+            if nlmsg_type == 3:  # NLMSG_DONE
+                assert not full_resp, full_resp
+                break
+            if not full_resp:
+                full_resp = s.recv(4096)
+            assert nlmsg_type == RTM_NEWADDR, (nlmsg_type, resp[:32])
+            # ifaddrmsg
+            ifa_family = int.from_bytes(resp[0:1], 'little', signed=False)
+            ifa_index = int.from_bytes(resp[4:8], 'little', signed=False)
+            resp = resp[8:]
+            while resp:
+                # rtattr
+                rta_len = int.from_bytes(resp[0:2], 'little', signed=False)
+                rta_type = int.from_bytes(resp[2:4], 'little', signed=False)
+                data = resp[4:rta_len]
+
+                if rta_type == 1:  # IFLA_ADDRESS
+                    if ifa_family == socket.AF_INET:
+                        ip = '.'.join('%d' % c for c in data)
+                        ipv4.append(ip)
+                    if ifa_family == socket.AF_INET6:
+                        ip = ':'.join(('%02x%02x' % (chunk[0], chunk[1]) if chunk != b'\0\0' else '') for chunk in
+                                      [data[0:2], data[2:4], data[4:6], data[6:8], data[8:10], data[10:12], data[12:14],
+                                       data[14:16]])
+                        ipv6.append(ip)
+                if rta_type == 3:  # IFLA_IFNAME
+                    name = data.rstrip(b'\0').decode()
+                    # print(ifa_index, name)
+
+                # need to round up to multiple of 4
+                if rta_len % 4:
+                    rta_len += 4 - rta_len % 4
+                resp = resp[rta_len:]
+        s.close()
+    else:
+        raise OSError("Unsupported operating system")
 
     return ipv4, ipv6
 
 
 def get_all_ipv4() -> list:
     return get_ips()[0]
-
-
-def open_url_(url):
-    os.system(f'start "" {url}')
 
 
 def open_url(url):
@@ -44,10 +104,10 @@ def open_url(url):
 
     Example: open_url("http://192.168.225.137:5555")
     """
-    try:
-        webbrowser.open(url, new=2)  # new=2 opens in a new tab, if possible
-    except Exception as e:
-        print(f"Error opening URL: {e}")
+    if platform.system() == "Windows":
+        os.system(f'start "" {url}')
+    else:
+        raise OSError("Unsupported operating system")
 
 
 def close_port_(ip, port):

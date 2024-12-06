12af1a5160180c2f0a739a4f086c757d89b6528e
j500 <C026730@cht-dm.cht.canon.co.th>
2024-12-06 18:32:53 +0700

    0.10.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hexss',
-    version='0.10.6',
+    version='0.10.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],

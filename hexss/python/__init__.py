3a5fdece324cb1327a932bd0320cbe5c173befc1
j500 <C026730@cht-dm.cht.canon.co.th>
2024-12-06 10:37:19 +0700

    0.10.5

--- hexss/python/__init__.py
+++ hexss/python/__init__.py
@@ -1,12 +1,21 @@
 import subprocess
 import sys
 
+pkg_name = {
+    'pygame-gui': 'pygame_gui'
+}
+
 
 def check_packages(*args):
     text = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'], text=True)
     installed_packages = {package.split('==')[0] for package in text.splitlines()}
 
-    missing_packages = [pkg for pkg in args if pkg not in installed_packages]
+    missing_packages = []
+    for pkg in args:
+        if pkg in pkg_name:
+            pkg = pkg_name[pkg]
+        if pkg not in installed_packages:
+            missing_packages.append(pkg)
 
     if missing_packages:
         raise ImportError(f"Missing packages; You can install them using `pip install {' '.join(missing_packages)}`")

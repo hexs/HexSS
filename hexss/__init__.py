79af1c89f708a08eba3f6edac4de6e7e0c935cc1
hexss <zxjq97@gmail.com>
2025-05-22 16:34:22 +0700

    0.19.0

--- hexss/__init__.py
+++ hexss/__init__.py
@@ -65,7 +65,7 @@ def initialize_proxies() -> Optional[Dict[str, str]]:
         return {}
 
 
-__version__ = '0.18.1'
+__version__ = '0.19.0'
 hostname = get_hostname()
 username = get_username()
 hexss_dir = get_hexss_dir()
